50f09f0c251daae0c38010dea1b55c64e89dc2f9
Alfredo Barajas Enriquez <axb0089@mavs.uta.edu>
2023-05-30 15:30:56 -0500

    AAA

---
+++ Alfredo/Testing.Code/testcode.ino
@@ -0,0 +1,61 @@
+const int analogPin = A2;  // ADC input pin
+const int numSamples = 50; // Number of samples to take
+
+// Conversion equation constants
+const float slope = 1.2617;
+const float intercept = 41.132;
+
+// Desired temperature and time constants
+const float desiredTemperature = 50.0; // Desired temperature in Celsius
+const float timeConstant = 0.1;       // Time constant for approaching the desired temperature
+
+float averageTemperature = 0.0; // Average temperature based on previous measurements
+
+void setup() {
+  Serial.begin(9600);  // Initialize serial communication
+  analogReference(DEFAULT); // Set the default reference voltage (5V)
+}
+
+void loop() {
+  int total = 0;
+
+  // Take 50 samples and sum them up
+  for (int i = 0; i < numSamples; i++) {
+    total += analogRead(analogPin);
+    delay(10); // Delay between each sample (adjust if needed)
+  }
+
+  // Calculate the average ADC value
+  float average = (float)total / numSamples;
+
+  // Convert ADC value to temperature
+  float temperature = slope * average + intercept;
+
+  // Update average temperature using previous measurements and time constant
+  averageTemperature = (averageTemperature * (timeConstant - 1.0) + temperature) / timeConstant;
+
+  // Print the average ADC value
+  Serial.print("Average ADC value: ");
+  Serial.println(average);
+
+  // Print the temperature in Celsius
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  // Calculate time remaining to reach the desired temperature
+  float timeRemaining = (desiredTemperature - averageTemperature) * timeConstant;
+
+  // Convert time remaining to minutes and seconds
+  int minutes = int(timeRemaining) / 60;
+  int seconds = int(timeRemaining) % 60;
+
+  // Print the remaining time in minutes and seconds
+  Serial.print("Time Left: [");
+  Serial.print(minutes);
+  Serial.print(" min. ");
+  Serial.print(seconds);
+  Serial.println(" sec.]");
+
+  delay(1000);  // Delay before taking the next set of samples
+}
